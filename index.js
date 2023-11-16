d562059df71f77a403fa49e612efd69e0d92e443
XandaoTP <98927676+XandaoTP@users.noreply.github.com>
2023-11-15 22:15:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const service = require('./src/service');
 
-const porta = process.env.PORTA || 3000;
+const port = process.env.PORTA || 3000;
 
 const listener = service.listen(porta, function () {
   console.log("Node.js listening on port " + listener.address().port);
