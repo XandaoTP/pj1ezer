df4f21a95e31557bc0c79c7eaed7da6bdcdc090f
XandaoTP <98927676+XandaoTP@users.noreply.github.com>
2023-11-16 23:07:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const service = require('./src/service');
 
 const port = process.env.PORTA || 3000;
 
-const listener = service.listen(porta, function () {
+const listener = service.listen(port, function () {
   console.log("Node.js listening on port " + listener.address().port);
 });
 
